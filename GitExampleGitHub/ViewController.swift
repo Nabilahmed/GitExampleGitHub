cc7563092424d69f96df58f1e107ecdd3bb0aee0
iNabilYE <nabilbloger@gmail.com>
2018-10-05 05:51:26 +0200

    Just add printing statement to try this

--- GitExampleGitHub/ViewController.swift
+++ GitExampleGitHub/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("just printing lol")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
